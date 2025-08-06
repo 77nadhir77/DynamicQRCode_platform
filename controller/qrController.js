83969cfcbb15ac5e15218cb0dff082e07e2c6826
nadir.NACEF <107315189+77nadhir77@users.noreply.github.com>
2025-08-06 03:34:49 +0100

    code qr modefication

--- controller/qrController.js
+++ controller/qrController.js
@@ -4,6 +4,7 @@ const { createCanvas, loadImage } = require("canvas");
 require("dotenv").config();
 const cloudinary = require("../utils/cloudinaryConfig").cloudinary;
 const streamifier = require("streamifier");
+const QRCodeSVG = require("qrcode-svg");
 
 exports.createQRCode = async (req, res) => {
   try {
@@ -15,50 +16,52 @@ exports.createQRCode = async (req, res) => {
       const newQRCode = await QRCode.create({
         link: redirectUrl,
       });
-      let svgData = await qrcode.toString(
-        `${process.env.QRCODE_LINK}/${newQRCode.id}`,
-        { type: "svg" }
-      );
+      // let svgData = await qrcode.toString(
+      //   `${process.env.QRCODE_LINK}/${newQRCode.id}`,
+      //   { type: "svg" }
+      // );
+
+      const qr = new QRCodeSVG({
+        content: `${process.env.QRCODE_LINK}/${newQRCode.id}`,
+        padding: 3,
+        width: 700,
+        height: 700,
+        color: "#000000",
+        background: "transparent",
+      });
+      let svgString = qr.svg();
+      
+      
+       const svgBase64 = `data:image/svg+xml;base64,${Buffer.from(
+        svgString
+      ).toString("base64")}`;
+
       const canvasSize = 900;
       const ctxMargin = 50; // how much inside the QR we draw the ID
       const canvas = createCanvas(canvasSize, canvasSize);
       const ctx = canvas.getContext("2d");
 
       // Load and draw the QR code image (full size)
+      //const logo = await loadImage("https://res.cloudinary.com/dpxpmkxhw/image/upload/v1754446721/uploads/uhalnszbafahgaw3lsic.png")
 
       // Add required width and height to make it renderable by canvas
 
-      if (!svgData.includes("width") && !svgData.includes("height")) {
-        svgData = svgData.replace(
-          "<svg",
-          `<svg width="${canvasSize}" height="${canvasSize}"`
-        );
-      }
-      const svgBase64 = `data:image/svg+xml;base64,${Buffer.from(
-        svgData
-      ).toString("base64")}`;
-
-
-
-      const logo = await loadImage("https://res.cloudinary.com/dpxpmkxhw/image/upload/v1754437588/uploads/uwuewiyttqispwb7eszi.png");
-      console.log("Loaded logo dimensions:", logo.width, logo.height);
-
-      const logoSize = canvasSize * 0.2;
-      const dx = (canvasSize - logoSize) / 2;
-      const dy = (canvasSize - logoSize) / 2;
+      // const logoSize = canvasSize * 0.2;
+      // const dx = (canvasSize - logoSize) / 2;
+      // const dy = (canvasSize - logoSize) / 2;
 
       const qrImage = await loadImage(svgBase64);
       ctx.drawImage(qrImage, 0, 0, canvasSize, canvasSize);
-      // Optional: white rounded box behind logo
-      ctx.fillStyle = "white";
-      ctx.beginPath();
-      ctx.roundRect
-        ? ctx.roundRect(dx, dy, logoSize, logoSize, 10)
-        : ctx.rect(dx, dy, logoSize, logoSize); // fallback if roundRect not supported
-      ctx.fill();
+      // // Optional: white rounded box behind logo
+      // ctx.fillStyle = "white";
+      // ctx.beginPath();
+      // ctx.roundRect
+      //   ? ctx.roundRect(dx, dy, logoSize, logoSize, 10)
+      //   : ctx.rect(dx, dy, logoSize, logoSize); // fallback if roundRect not supported
+      // ctx.fill();
 
       // Draw the logo
-      ctx.drawImage(logo, dx, dy, logoSize, logoSize);
+      //.drawImage(logo, dx, dy, logoSize, logoSize);
 
       // Format ID (4 digits minimum)
       const idText =
@@ -69,7 +72,7 @@ exports.createQRCode = async (req, res) => {
       // Prepare text style
       ctx.save();
       ctx.font = "bold 36px Arial"; // larger font
-      ctx.fillStyle = "black";
+      ctx.fillStyle = "black"; // text color
       ctx.textAlign = "center";
 
       // Move origin to slightly inside the right side of QR and center vertically
